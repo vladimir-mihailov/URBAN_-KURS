0aa5b4d8f6d8d709d75192ed4f24d5640e141e34
vladimir-mihailov <you@example.com>
2025-01-08 13:26:52 +0400

    Module_2_2

--- module_2_2.py
+++ module_2_2.py
@@ -1,12 +1,14 @@
-first = int(input("Число: "))
-second = int(input("Число: "))
-third = int(input("Число: "))
+first = int(input("first: "))
+second = int(input("second: "))
+third = int(input("third: "))
 print(f'first: {first}, second: {second}, third: {third}')
 
-if first==second==third:
-    print(3)
-elif not first != second == third or first == second != third or first!=third==second:
+#if first == second != third or third == second != first or first!=third==second:
+if(first==second and second!=third) or (second==first and first!=third) or (first==third and first!=second):
     print(2)
-elif not first==second==third:
+elif first==second==third:
+    print(3)
+#elif first!=third and first!=second and third!=second:
+else:
     print(0)
 
