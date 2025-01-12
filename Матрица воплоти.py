25f70be42a2ae314ba383b9b58493e80abd02dcc
vladimir-mihailov <you@example.com>
2025-01-12 15:51:57 +0400

    Функции в Python.Функция с параметром

---
+++ Матрица воплоти.py
@@ -0,0 +1,18 @@
+def get_matrix(n, m, value):
+    if n <= 0 or m <= 0:
+        return []
+    matrix = []
+    for i in range(n):
+        row = []
+        for d in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+
+result_1 = get_matrix(2, 2, 10)
+result_2 = get_matrix(3, 5, 42)
+result_3 = get_matrix(4, 2, 13)
+
+print(result_1)
+print(result_2)
+print(result_3)
