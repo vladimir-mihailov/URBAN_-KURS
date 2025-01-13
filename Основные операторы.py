6e09d8362cbc0841eabb2d3f75c82216f5d50823
vladimir-mihailov <you@example.com>
2025-01-13 12:23:20 +0400

    Дополнительное практическое задание по модулю* ///

--- Основные операторы.py
+++ Основные операторы.py
@@ -5,18 +5,20 @@ def generat_password(first_insert):
     for i in range(1, first_insert):
         for a in range(i + 1, first_insert + 1):
             pairs.append((i, a))
-        # Проверка кратности
+        # Проверка кратности и формирование пароля
         for pair in pairs:
             sum_pair = sum(pair)
             if first_insert % sum_pair == 0:
                 password += f"{pair[0]}{pair[1]}"
 
-        return password
-#  вызов функции
+        return password # Вырезаем до 42 символов, если нужно
+
+#  вызов функции:
 first_insert = int(input("Введите число от 3 до 20: "))
 if 3 <= first_insert <= 20:
     reult = generat_password(first_insert)
-    print(f"Пароль для числа {first_insert}:{reult}")
+    print(f"Паролем числа {first_insert} является {reult}")
 else:
     print(f"Ошибка: повторите ввод (от 3 до 20) ")
+
 
